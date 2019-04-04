b628d188206aace454a262d33d94e73f093d0fc9
D7703_22 <d7703_22@promise.local>
2019-04-04 16:51:45 +0900

    commit

--- UITextField Test 02/ViewController.swift
+++ UITextField Test 02/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        print(myTextField.text!)    //!넣어얗ㄹ
+        let inString = myTextField.text
+        let outString = inString! + " Coding"
+        myLabel.text = outString
+        myTextField.text = ""
+        myTextField.resignFirstResponder()  //키보드 강제로 내림
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)   //화면 터치하면 하고있는거 끝냄 => 바탕화면 클릭시 키보드 내려감.
+        myTextField.backgroundColor = UIColor.yellow
+        view.backgroundColor = UIColor.green
+        
+        
+    }
 }
 
