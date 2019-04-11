91c54885b25631367fe91c038683334bb56bc61e
ybh <2616442@naver.com>
2019-04-11 16:59:56 +0900

    Project Test

--- UITextField Test 02/ViewController.swift
+++ UITextField Test 02/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 //delegate 1 => UI의 delegate 객체 상속.
 class ViewController: UIViewController, UITextFieldDelegate{
+    
     @IBOutlet weak var myTextField: UITextField!
     @IBOutlet weak var myLabel: UILabel!
+    
+    var number = 0
+    var isprime = true
+    var key = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //delegate 3 => delegate 객체와 ciewcontroller 객체 연결
@@ -19,41 +25,73 @@ class ViewController: UIViewController, UITextFieldDelegate{
         
         //textField에 x표시 뜨게 나옴
         myTextField.clearButtonMode = UITextField.ViewMode.always
+        myTextField.placeholder = "숫자를 입력하세요"        //초기에 textField에 글자 나옴
         
-        //초기에 textField에 글자 나옴
-        myTextField.placeholder = "숫자를 입력하세요"
+        
+        
     }
 
     @IBAction func buttonPressed(_ sender: Any) {
-        print(myTextField.text!)    //!넣어얗ㄹ
-        let inString = myTextField.text
-        let outString = inString! + " Coding"
-        myLabel.text = outString
-        myTextField.text = ""
-        myTextField.resignFirstResponder()  //키보드 강제로 내림
+        
+        if let number = Int(myTextField.text!) {
+            print("TRUE \(number)")
+            
+            if number == 0 {
+                print("input again!")
+                return
+            }
+            ////
+            print("check 과정시작")
+            
+        } else {
+
+            print("FALSE \(number)")
+            print("again!! numeric")
+
+        }
+        
+        
+        /*
+        if number <= 1{
+            isprime = false
+        }
+        
+        
+        var i = 2
+        while i < number{
+            if number % i == 0{
+                isprime = false
+            }
+            i += 1
+        }
+        
+        if isprime == true{
+            myLabel.text = "Prime Number"
+        }
+        else{
+            myLabel.text = "Not Prime Number"
+        }
+        
+        if let value = number {
+            myLabel.text = "올바른 숫자를 입력하시오"
+        }
+        if let number =  {
+            myLabel.text = "숫자를 입력하시오"
+        }
+ */
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)   //화면 터치하면 하고있는거 끝냄 => 바탕화면 클릭시 키보드 내려감.
-        myTextField.backgroundColor = UIColor.yellow
-        view.backgroundColor = UIColor.green
-        
-        
     }
     
     //delegate 2 => Delegate 메소드 overriding
-    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.view.backgroundColor = UIColor.green
-        //키패드 내리기
-        myTextField.backgroundColor = UIColor.yellow
-        return true
-    }
+//    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        if key==false{
+//            return true
+//        }
+//    }
     
-    //
-    public func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        myTextField.backgroundColor = UIColor.lightGray
-
-        return true
-    }
 }
 
